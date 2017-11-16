2fbbc5d985d334a49a0d3f8c935d0461cb619474
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-16 21:36:18 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,14 @@
 def key_for_min_value(name_hash)
 compare=10000
 output=10000
-  name_hash.each do |key, value|
-    if compare>name_hash[key]
-    compare=name_hash[key]
-    output=key
+  if name_hash
+    name_hash.each do |key, value|
+      if compare>name_hash[key]
+      compare=name_hash[key]
+      output=key
+      end
     end
-  end
+  else puts nil
+  end  
   output
 end
