4a223da9e79b97b7692d4ed6f5187233ba159148
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-16 21:14:47 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  output= name_hash[name][0]
+    name_hash.each do |name,val|
+      if  output > name_hash[name][0]
+        output= name_hash[name][0]
+      end
+    end
+    return output
 end
